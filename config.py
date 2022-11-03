60b66a3da2912d6dfdce6f791c7c7e369d5ef675
Bănaru M. Sebastian <mircea.banaru@stud.etti.upb.ro>
2022-11-03 23:33:52 +0200

    .

--- config.py
+++ config.py
@@ -5,15 +5,18 @@ from imports import *
  # PATHS #
 ###########
 
-PATH = 'D:\\se_joaca_licenta\\Datasets\\CelebA\\Img\\images_cropped_png\\img_align_celeba_png\\'
+PATH = 'D:\\se_joaca_licenta\\Datasets\\CelebA\\Img\\images_non_cropped\\img_celeba\\'
 SAVE_MODEL_PATH = 'C:\\Users\\vlogw\\Desktop\\licenta\\BNRNet_models\\first_experiment_working_vgg\\'
 MODEL_PATH = None
 
+
+LOAD_MODEL_PATH = 'C:\\Users\\vlogw\\Desktop\\licenta\\BNRNet_models\\first_experiment_working_vgg\\'
+INFER_PATH = 'D:\\se_joaca_licenta\\Datasets\\CelebA\\Img\\images_non_cropped\\img_celeba\\'
 
 ###########
  # PARAMS #
 ###########
 
 
-batch_size = 8
+batch_size = 10
 number_of_epochs = 20
