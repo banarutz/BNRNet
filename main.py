9c306b91d45a9db2b7665bebe6b4a05c493948bb
Bănaru M. Sebastian <mircea.banaru@stud.etti.upb.ro>
2022-11-06 01:13:55 +0200

    Validation added

--- main.py
+++ main.py
@@ -9,33 +9,27 @@ def load_dataset(PATH):
 	train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
 	return train_loader
 
+def validation_dataset(VALIDATION_PATH):
+    train_set = VGGLoader(VALIDATION_PATH)
+    train_loader = DataLoader(dataset=train_set, batch_size=1, shuffle=True)
+    return train_loader
 
-def train (train_loader, optimizer, model, epoch):
+
+def train (train_loader, valid_loader, optimizer, model, epoch):
     model = torch.nn.DataParallel(model).cuda()
     model.train()
     train_loss = []
-    
+    print('### Validation ###')
     for iteration, batch in (enumerate(tqdm.auto.tqdm(train_loader))):
         torch.cuda.empty_cache()
         optimizer.zero_grad()
         input, label = batch
         input, label = input.to("cuda"), label.to("cuda")
         predict = model(input)
-        # predict = predict.cuda()
-        # label = label.cuda()
-
-        # predict = to_numpy(predict)
-        # label = to_numpy(label)
-
-        # print(label.shape)
 
         MSE = torch.nn.L1Loss ()
         mse = MSE (predict, label)
-        # mse = get_mse(predict[:,0,:,:] , label[:,0,:,:])
-        # mse += get_mse(predict[:,1,:,:] , label[:,1,:,:]) 
-        # mse = np.array(mse)
-        # mse = torch.tensor(mse, requires_grad = True)
-        # mse = Variable(mse, requires_grad = True)
+
         mse.backward()
         optimizer.step()
         mse = to_numpy(mse)
@@ -44,14 +38,30 @@ def train (train_loader, optimizer, model, epoch):
         #####DETACH AICI################
         #  
 
-    # model.eval()
-    # with torch.no_grad():
-    #     print('am ajuns aici')
+
 
  ################ VALIDARE ###################
+    model.eval()
+    mae_valid_loss =[]
+    mse_valid_loss =[]
+    with torch.no_grad():
+        for iteration, batch in (enumerate(tqdm.auto.tqdm(train_loader))):
+            input, label = batch
+            input, label = input.to("cuda"), label.to("cuda")
+            predict = model(input)
 
-    # return np.mean(train_loss)
-    return np.mean(train_loss)
+            MAE = torch.nn.L1Loss ()
+            mae = MAE (predict, label)
+            mae = to_numpy(mae)
+            mae_valid_loss.append(mae)
+
+            MSE = torch.nn.MSELoss ()
+            mse = MSE (predict, label)
+            mse = to_numpy(mse)
+            mse_valid_loss.append(mse)
+
+    #     print('am ajuns aici')
+    return np.mean(train_loss), np.mean(mse_valid_loss), np.mean(mae_valid_loss)
 
 
    
@@ -76,14 +86,18 @@ def main():
     
 
     train_loader = load_dataset(PATH)
+    valid_loader = load_dataset(VALIDATION_PATH)
     optimizer = torch.optim.Adam(list(model.parameters()), lr = 10**-(5))
-    epoch_loss = []
+
     writer = SummaryWriter()
     for epoch in tqdm.tqdm (range(start_epoch, start_epoch + number_of_epochs)): 
-        mse = train (train_loader, optimizer, model, epoch)
-        writer.add_scalar('Train loss -> MSE', mse, epoch)
+        train_loss, mse_valid, mae_valid = train (train_loader, valid_loader, optimizer, model, epoch)
 
-        print('Epoch:', epoch, 'has mse:', mse)
+        writer.add_scalar('Train loss -> MSE', train_loss, epoch)
+        writer.add_scalar('MAE validation', mae_valid, epoch)
+        writer.add_scalar('MSE validation', mse_valid, epoch)
+
+        print('Epoch:', epoch, 'has mae:', train_loss, 'valid_mae:', mae_valid, 'valid_mse:', mse_valid)
         print('########################')
         # epoch_loss.append(mse)
 
@@ -95,7 +109,7 @@ def main():
                         'epoch': epoch,
                         'state_dict': model.state_dict(),
                         'optimizer_state_dict': optimizer.state_dict(),
-                        'loss': mse}, 
+                        'loss': train_loss}, 
                         model_path)
     
 
